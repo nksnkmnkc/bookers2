beeb49b556d38fa6527f93f9cefd68872934fa8e
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-10-20 12:10:02 +0000

    [Add]header.footer.top.aboutのレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
+  get "homes/about" => "homes#about", as: "about"
+
+  resources :books, only:[:new, :index, :show, :edit ,:updata, :destroy]
+  resources :users, only:[:show, :edit, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
