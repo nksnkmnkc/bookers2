a0253e28d83baa579e1f403f8dbade436367a9f0
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-10-23 01:24:06 +0000

    [Add]bookのindex,showとuserのindex,show修正 　フラッシュメッセージ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,13 @@ class UsersController < ApplicationController
   end
  end
 
+ def create
+   @user = User.new(user_params)
+    if @user.save
+     flash[:notice] = 'Welcome!You have signed up successfully.'
+     redirect_to user_path
+    end
+ end
 
  def update
   @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about", as: "about"
 
   resources :books, only:[:new, :index, :show, :edit , :destroy, :create]
-  resources :users, only:[:show, :edit, :index ,:update]
+  resources :users, only:[:show, :edit, :index ,:update, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
