a19462a0541aa705ba38446661d3527f24792e75
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-10-23 18:47:47 +0000

    [Fix]RSpecテストSTEP1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,10 @@ class BooksController < ApplicationController
   @book.user_id = current_user.id
   if @book.save
    flash[:notice] = "You have created book successfully."
-   redirect_to books_path
+   redirect_to book_path(@book)
   else
+   @user = current_user
+   @books = Book.all
    render :index
   end
  end
@@ -24,33 +26,40 @@ class BooksController < ApplicationController
  def show
   @book = Book.find(params[:id])
   @books = Book.new
-  @user = current_user
+  @user = @book.user
  end
 
  def edit
   @book = Book.find(params[:id])
+  if @book.user == current_user
+   render "edit"
+  else
+   redirect_to books_path
+  end
  end
 
  def update
-  book = Book.find(params[:id])
-  if book.update(book_params)
+  @book = Book.find(params[:id])
+  @book.user_id == current_user.id
+  if @book.update(book_params)
    flash[:notice] = "You have update book successfully."
-   redirect_to book_path(book.id)
+   redirect_to book_path
   else
    render :edit
   end
  end
 
  def destroy
-  book = Book.find(params[:id])
-  book.destroy
+  @book = Book.find(params[:id])
+  if @book.destroy
   redirect_to books_path
+  end
  end
 
  private
 
  def book_params
-  params.require(:book).permit(:title, :body, :image)
+  params.require(:book).permit(:title, :body)
  end
 
 end
